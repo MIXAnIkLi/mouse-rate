e3dc0cb5db1fdeddb4bd6834f516ef0ed78d135e
MIXAnIkLi <98084328+MIXAnIkLi@users.noreply.github.com>
2024-12-25 11:37:16 +0300

    End1

--- src/components/Layout/SiderLayout.jsx
+++ src/components/Layout/SiderLayout.jsx
@@ -8,30 +8,34 @@ import {
 } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
 import PropTypes from "prop-types";
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useParams,  } from 'react-router-dom';
 
 export default function SiderLayout({ collapsed, setCollapsed }) {
   const { Sider } = Layout;
-
+  const params = useParams();
+  console.log(params)
   function getItem(label, key, icon, to, children) {
     return {
       key,
       icon,
       children,
-      label: <Link to={to}  >{label}</Link>,
+      label: <Link to={to}>{label}</Link>,
     };
   }
   const items = [
-    getItem("Главная страница", "1", <DesktopOutlined />, "/"),
-    getItem("Бланк игрока", "2", <FileTextOutlined />, "/player-form"),
-    getItem("Правила игры", "3", <PieChartOutlined />, "/rules"),
-    getItem("Добавить игрока", "4", <BranchesOutlined />, "/add-player"),
+    getItem("Главная страница", "/", <DesktopOutlined />, "/"),
+    getItem("Бланк игрока", "/player-form", <FileTextOutlined />, "/player-form"),
+    getItem("Правила игры", "/rules", <PieChartOutlined />, "/rules"),
+    getItem("Добавить игрока", "/add-player", <BranchesOutlined />, "/add-player"),
     getItem("Игроки", "sub1", <UserOutlined />, null, [
-      getItem("Миша", "5", null, "/players/1"),
-      getItem("Ксюша", "6", null, "/players/2"),
+      getItem("Миша", "/players/1", null, "/players/1"),
+      getItem("Ксюша", "/players/2", null, "/players/2"),
     ]),
     getItem("Скачать бланк", "9", <FileOutlined />, "/download-form"),
   ];
+
+  
 
   return (
     <>
@@ -40,12 +44,17 @@ export default function SiderLayout({ collapsed, setCollapsed }) {
         collapsible
         collapsed={collapsed}
         onCollapse={(value) => setCollapsed(value)}
-        style={{ position: "sticky", top: 0, height: "100vh", overflow: "auto" }}        
+        style={{
+          position: "sticky",
+          top: 0,
+          height: "100vh",
+          overflow: "auto",
+        }}
       >
         <div className="demo-logo-vertical" />
         <Menu
           theme="dark"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={[location.pathname]}
           mode="inline"
           items={items}
           style={{ height: "100%" }}
@@ -58,5 +67,4 @@ export default function SiderLayout({ collapsed, setCollapsed }) {
 SiderLayout.propTypes = {
   collapsed: PropTypes.bool.isRequired,
   setCollapsed: PropTypes.func.isRequired,
-  activeLink:  PropTypes.string.isRequired,
 };
